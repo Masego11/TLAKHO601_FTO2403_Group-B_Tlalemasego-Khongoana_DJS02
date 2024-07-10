cb28638046b5c8c84fea3da17e1b0d0c8e31df52
Tlalemasego <tlalemasego@gmail.com>
2024-07-10 12:27:38 +0200

    git: Ensured divided numbers don't result in decimals.

--- scripts.js
+++ scripts.js
@@ -5,5 +5,6 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  const divisionResult = dividend / divider; // declared the divisionResult to handle the calculation
+  result.innerText = Math.trunc(divisionResult); //called the Match.trunc which truncates the fractionalpart and assigned the divisionResult variable in its parameters. 
 });
